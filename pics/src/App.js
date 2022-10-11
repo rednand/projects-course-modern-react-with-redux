cd794337eea7ac3d0150f3ae35476ffce418d686
Renan de Aguiar Modesto <Renan de Aguiar Modesto>
2022-10-11 17:06:39 -0300

    ajuste app foto

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { SearchBar } from "./components/SearchBar";
+import { ImageList } from "./components/ImageList";
 import api from "./services/api";
 
 class App extends React.Component {
@@ -17,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,11 @@
+import "./ImageList.css";
+import React from "react";
+import { ImageCard } from "./ImageCard";
+
+export const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+
+  return <div className="image-list">{images}</div>;
+};
